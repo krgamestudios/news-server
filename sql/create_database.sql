4d71b1805c09c552ee261c84d5108bbac9375975
Kayne Ruse <kayneruse@gmail.com>
2021-02-04 00:42:33 +0000

    Getting sick of docker

--- sql/create_database.sql
+++
@@ -1,9 +0,0 @@
-#This file only needs to be run once, during initial setup
-
-#Create the actual database
-CREATE DATABASE IF NOT EXISTS news;
-USE news;
-
-#Create the database user
-CREATE USER IF NOT EXISTS 'news'@'%' IDENTIFIED BY 'charizard';
-GRANT ALL PRIVILEGES ON news.* TO 'news'@'%';
