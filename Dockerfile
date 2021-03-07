7de1083fddbc19a9a6aa3152719f93a168c3e427
Kayne Ruse <kayneruse@gmail.com>
2021-03-07 15:34:35 +1100

    Finally playing nice together

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,10 @@
+
 FROM node:15
-
-# Change working directory
 WORKDIR "/app"
-
-# Copy package.json and package-lock.json
 COPY package*.json ./
-
-# Install npm production packages 
 RUN npm install --production
-
 COPY . /app
-
 EXPOSE 3100
-
 USER node
-
 ENTRYPOINT ["bash", "-c"]
 CMD ["sleep 10 && npm start"]
--- configure-script.js
+++ configure-script.js
@@ -123,7 +123,7 @@ CMD ["sleep 10 && npm start"]
 	const sqlfile = `
 CREATE DATABASE IF NOT EXISTS ${appName};
 CREATE USER IF NOT EXISTS '${appDBUser}'@'%' IDENTIFIED BY '${appDBPass}';
-GRANT ALL PRIVILEGES ON ${appName}.* TO '${appDBUser}'@'%';	
+GRANT ALL PRIVILEGES ON ${appName}.* TO '${appDBUser}'@'%';
 `;
 
 	fs.writeFileSync('docker-compose.yml', ymlfile);
